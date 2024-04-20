91bb7abb4eb2d5555b1ebe04f111de93511da254
PhyShock <Fedor.Shkolnikov@t-systems.com>
2024-04-20 11:59:41 +0200

    pages removed & calendar added

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,32 +1,51 @@
-import type { FC } from 'react';
+import { type FC, useMemo, useState } from 'react';
+import { useInitData } from '@tma.js/sdk-react';
+import { Calendar } from 'react-calendar';
 
-import { Link } from '~/components/Link/Link.tsx';
+import { User, postEvent } from '@tma.js/sdk';
 import { Page } from '~/components/Page/Page.tsx';
-import { routes } from '~/navigation/routes.tsx';
 
 import './IndexPage.css';
 
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+interface Register {
+  id: string;
+  name: string;
+  phone: string;
+  date: string;
+}
+
+function getUserRows(user: User): Register {
+  return {
+    id: user.id.toString(),
+    name: user.firstName + ' ' + user.lastName,
+    phone: '',
+    date: ''
+  }
+}
+
 export const IndexPage: FC = () => {
+  const initData = useInitData();
+  const [value, onChange] = useState<Value>(new Date());
+
+  const userRows = useMemo<Register | undefined>(() => {
+    return initData && initData.user ? getUserRows(initData.user) : undefined;
+  }, [initData]);
+
+
+  postEvent('web_app_send_data', "123");
+
   return (
     <Page title="Home Page">
       <p>
         This page is a home page in this boilerplate. You can use the links below to visit other
         pages with their own functionality.
       </p>
-      <ul className="index-page__links">
-        {routes.map(({ path, title, icon }) => title && (
-          <li className="index-page__link-item" key={path}>
-            <Link className="index-page__link" to={path}>
-              {icon && (
-                <i className="index-page__link-icon">
-                  {icon}
-                </i>
-              )}
-              {title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <Calendar onChange={onChange} value={value} />
     </Page>
   );
 };
