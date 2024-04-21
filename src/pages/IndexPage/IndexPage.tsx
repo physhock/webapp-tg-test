bc478471ce68f08184c8ebfc8c2a387913ae331d
PhyShock <fedorsh@yandex.ru>
2024-04-21 14:08:42 +0200

    register obj added

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,17 +1,16 @@
-import { useState, type FC } from 'react';
+import { useMemo, useState, type FC } from 'react';
 import { Calendar } from 'react-calendar';
 
 import { Page } from '~/components/Page/Page.tsx';
 
 import './IndexPage.css';
-import { useMiniApp } from '@tma.js/sdk-react';
+import { useInitData, useMiniApp } from '@tma.js/sdk-react';
+import { User } from '@tma.js/sdk';
 
 
 
 type ValuePiece = Date | null;
-
 type Value = ValuePiece | [ValuePiece, ValuePiece];
-
 
 const mapFighterToDates: Map<string, Array<string>> = new Map([
   ['Conor McGregor', ['2024-04-20', '2024-04-19', '2024-04-15']],
@@ -19,44 +18,40 @@ const mapFighterToDates: Map<string, Array<string>> = new Map([
   ['Fedor Emelianenko', ['2024-04-24', '2024-04-14', '2024-04-13']]
 ])
 
+interface Register {
+  id: string;
+  name: string;
+  phone: string;
+  date: string;
+  fighter: string;
+}
 
-// interface Register {
-//   id: string;
-//   name: string;
-//   phone: string;
-//   date: string;
-// }
-
-// function getUserRows(user: User): Register {
-//   return {
-//     id: user.id.toString(),
-//     name: user.firstName + ' ' + user.lastName,
-//     phone: '',
-//     date: ''
-//   }
-// }
-
-
-
+function getUserRows(user: User): Register {
+  return {
+    id: user.id.toString(),
+    name: user.firstName + ' ' + user.lastName,
+    phone: '',
+    date: '',
+    fighter: ''
+  }
+}
 
 function isSameDay(dDate: string, date: Date): boolean {
-  console.log(date, dDate);
   return new Date(dDate).getDate() === date.getDate() && new Date(dDate).getMonth() === date.getMonth() && new Date(dDate).getFullYear() === date.getFullYear();
 }
 
 
 export const IndexPage: FC = () => {
+  const initData = useInitData();
   const [chosenFighter, setChosenFighter] = useState<string>('');
   const [date, onChange] = useState<Value>(new Date());
-
-
   const miniApp = useMiniApp();
   console.log('index' + miniApp.isRequestingPhoneAccess);
 
 
-  // function onChange(value: Value) {
-  //   miniApp.sendData(value?.toString() || '');
-  // }
+  const userRows = useMemo<Register | undefined>(() => {
+    return initData && initData.user ? getUserRows(initData.user) : undefined;
+  }, [initData]);
 
   function createButton() {
     const fighters = Array.from(mapFighterToDates.keys());
@@ -76,17 +71,28 @@ export const IndexPage: FC = () => {
     return false;
   }
 
-  return (
-    <Page title="Home Page">
-      <p>
-        This page is a home page in this boilerplate. You can use the links below to visit other
-        pages with their own functionality.
-      </p>
-      <div>{createButton()}</div>
-      {chosenFighter !== '' ? (
-        <Calendar onChange={onChange} value={date} tileDisabled={tileDisabled} />
-      ) : null}
 
+
+  return (
+    <Page title="Barbershop">
+      <p>
+        Hello, {userRows?.name}, welcome to the Barbershop! Choose a fighter to book an appointment with:
+      </p>
+      <div>
+        {createButton()}
+        {chosenFighter !== '' ? (
+          <>
+            <Calendar onChange={onChange} value={date} tileDisabled={tileDisabled} />
+            <button onClick={() => {
+              if (date && userRows) { // Add null check for userRows
+                userRows.fighter = chosenFighter;
+                userRows.date = date.toString();
+                miniApp.sendData(JSON.stringify(userRows)); // Convert userRows to JSON string
+              }
+            }}>'Book an appointment'</button>
+          </>
+        ) : null}
+      </div>
     </Page>
   );
 };
